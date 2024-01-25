e4ee9359285ee4d9c74aa6aea9de197ff4825944
Dheeraj Mahanidhi <dheeraj.beerelly@gmail.com>
2024-01-25 13:24:01 -0700

    Added Color Mode Switch to Toggle Btw Dark and Light Themes

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>
